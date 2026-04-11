1b9a23506b0ee7bc264ef48c4486ab3f25c0ae45
ankitsunil530 <22bcs244@iiitdmj.ac.in>
2026-04-11 17:42:49 +0530

    backend polished

---
+++ models/Comments.js
@@ -0,0 +1,22 @@
+import mongoose from "mongoose";
+const commentSchema = new mongoose.Schema({
+    taskId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Task",
+        required: true
+    },
+
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "User",
+        required: true
+    },
+
+    content: {
+        type: String,
+        required: true
+    }
+
+}, { timestamps: true });
+
+module.exports = mongoose.model("Comment", commentSchema);
